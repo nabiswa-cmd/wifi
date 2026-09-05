2c15e48ec4d34ea7959c85dd061431fbb8ba308d
NABISWA JAMES <nabiswaj8@gmail.com>
2026-09-05 19:47:36 +0300

    'all

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('DJANGO_SECRET_KEY', default='insecure-dev-key-change-me')
 DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS =['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
